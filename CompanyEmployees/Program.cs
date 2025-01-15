f671e9cc20bce4ba74c97dc9ecdc0cf553d5a398
david-lagrange <davidnlagrange@hotmail.com>
2025-01-15 00:22:05 -0600

    Enhance middleware and service configurations
    Added using directive for Microsoft.AspNetCore.HttpOverrides.
    Configured CORS and IIS integration services.
    Enabled HTTPS redirection and serving static files.
    Set up environment-specific error handling.
    Configured forwarded headers and CORS policy.

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,7 +1,9 @@
 using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 
@@ -10,8 +12,23 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
