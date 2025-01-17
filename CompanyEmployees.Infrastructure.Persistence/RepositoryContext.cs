8bdc87057f31ac0b5d84dc9e09d7f93198e4773f
david-lagrange <davidnlagrange@hotmail.com>
2025-01-17 00:02:05 -0600

    Add new projects and PostgreSQL support
    Updated solution to include new Core.Domain and Infrastructure.Persistence projects. Added PostgreSQL package and connection strings. Created Company and Employee entities. Implemented RepositoryContext and RepositoryContextFactory for database access.

---
+++ CompanyEmployees.Infrastructure.Persistence/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployees.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.Infrastructure.Persistence
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
